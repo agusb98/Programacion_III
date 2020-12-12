16a23fc0f70ed0040e4d8053831b2fd3b05dd741
agusb98 <agusszurdob@gmail.com>
2020-12-11 21:51:15 -0300

    -

--- 04-Clases/04/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "./Clases/token.php";
-
-$key = "333";
-$token = Token::GenerarToken($key);
-
-print_r(Token::ObtenerToken($token, $key));
-?>
--- 04-Clases/06/clases/jwt.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Clases;
-
-class JWT {
-    
-}
